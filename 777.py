95d4cf56237eeff237d23f2a469ef679c3817186
Pcmastercode <asap962007@gmail.com>
2018-02-11 19:50:23 +0500

    Add files via upload

---
+++ 777.py
@@ -0,0 +1,11 @@
+from math import *
+a = float
+b = float
+c = float
+print('Введите значение 1 катета')
+a = float(input())
+print('Введите значение 2 катета')
+b = float(input())
+c = float(sqrt((a**2) + (b**2)))
+print('Гипотенуза равна')
+print(c)
